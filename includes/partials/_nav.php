b3baaace3b74474136626220d594d26dbd32db76
marioashok95 <marioashok95@gmail.com>
2020-03-02 12:51:16 +0530

    Added a Navabr

--- includes/partials/_nav.php
+++ includes/partials/_nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">marioashok.com</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -6,8 +6,37 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="myprofile.php">My Profile</a>
       </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="dashboard.php">Dashboard</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="posts.php">Posts</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="categories.php">Categories</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="admins.php">Manage Admins</a>
+      </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="comments.php">Comments</a>
+      </li>
+
+          <li class="nav-item">
+        <a class="nav-link" href="blog.php">Blog</a>
+      </li>
+
+      
+
+
+
     </ul>
 
     <ul class="navbar-nav ml-auto">
